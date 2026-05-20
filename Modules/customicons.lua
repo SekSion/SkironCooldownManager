da50cbc1eec997720bf2cbb0d1820549d7eec4a1
Permok <martinw1394@gmail.com>
2026-05-20 19:37:29 +0200

    Don't desaturate custom slot icons when they are only on the GCD

--- Modules/customicons.lua
+++ Modules/customicons.lua
@@ -392,7 +392,10 @@ local function UpdateCustomIconCooldown(frame, iconType, config)
 		local startTime, duration = GetInventoryItemCooldown("player", config.slotID)
 		if startTime and startTime > 0 and (startTime + duration) - GetTime() >= 0.1 then
 			frame.Cooldown:SetCooldown(startTime, duration)
-			frame.Icon:SetDesaturated(true)
+
+			local globalCooldown = C_Spell.GetSpellCooldown(61304)
+			frame.Icon:SetDesaturated(not (duration == globalCooldown.duration))
+			
 			UpdateCustomIconGCD(frame, config, true)
 			UpdateCustomIconGlow(frame, false)
 			return true
