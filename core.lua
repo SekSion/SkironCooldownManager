21af7f3cf8283dd633c0f296b2d4ad563acd398b
Permok <martinw1394@gmail.com>
2026-06-22 16:28:55 +0200

    Clean up the resource bar file so it's more readable

--- core.lua
+++ core.lua
@@ -264,24 +264,9 @@ function SCM:TRAIT_CONFIG_UPDATED()
 	end)
 end
 
-local function ClearLayoutPoints()
-	if InCombatLockdown() then
-		return
-	end
-
-	for _, anchorFrame in pairs(SCM.anchorFrames) do
-		anchorFrame:ClearAllPoints()
-	end
-
-	SCM_ResourceBarContainer:ClearAllPoints()
-	SCM_SecondaryResourceBar:ClearAllPoints()
-	SCM_PrimaryResourceBar:ClearAllPoints()
-	SCM_CastBar:ClearAllPoints()
-end
-
 function SCM:ACTIVE_PLAYER_SPECIALIZATION_CHANGED()
 	SCM:ResetCooldownViewerRuntimeState()
-	ClearLayoutPoints()
+	SCM:ResetResourceBar()
 
 	C_Timer.After(0.5, function()
 		RefreshCooldownViewerData(true)
